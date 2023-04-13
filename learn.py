3519e189fd030f17ed2c7f4e098d273428caaa2f
ninhtonvinh99 <73169286+ninhtonvinh99@users.noreply.github.com>
2023-04-13 14:53:19 +0700

    Add files via upload

---
+++ learn.py
@@ -0,0 +1,68 @@
+#print("Hello")
+"""n = 1
+print(n)
+n = 2
+print(n)
+v =1
+del v
+print (v)
+
+ str = 'Hello World!'
+
+print(str)          # In ra toàn bộ chuỗi
+print(str[0])       # In phân tử đầu tiên của chuỗi
+print(str[2:5])     # In phần tử thứ 3 đến thứ 6, tại vì chỉ mục bắt đầu từ 0
+print(str[2:])      # In từ vị trí thứ 3 đến cuối chuỗi
+print(str * 2)      # In chuỗi hai lần
+print(str + "TEST") # Nối chuỗi
+
+list = [ 1, " Mai Xuan Ninh ", 1999]
+list[0] = 27
+print(list[0])
+
+list = [ 1, " Mai Xuan Ninh ", 1999]
+tuple = (2, 4 , list )
+# tuple[0] = 1
+tuple[2][1]= 'hello'
+print(tuple[2][1])
+
+Dic = {'name' : 'Mai Xuan Ninh', 'tuoi ' : 24, 'cong viec ' : 'engineer'}
+print(Dic['name'])
+Dic['tuoi'] = 23
+print(Dic['tuoi'])
+del Dic['cong viec']
+print (Dic['cong viec'])
+
+num_int = 123
+num_flo = 1.23
+
+num_new = num_int + num_flo
+
+print("Kiểu dữ liệu của num_int:",type(num_int))
+print("Kiểu dữ liệu của num_flo:",type(num_flo))
+
+print("Giá trị của num_new:",num_new)
+print("Kiểu dữ liệu của num_new:",type(num_new))
+
+num_int = 123
+num_str = "456"
+
+print("Kiểu dữ liệu của num_int:",type(num_int))
+print("Kiểu dữ liệu của num_str trước khi ép kiểu:",type(num_str))
+
+num_str = int(num_str)
+print("Kiểu dữ liệu của num_str sau khi ép kiểu:",type(num_str))
+
+num_sum = num_int + num_str
+
+print("Tổng của num_int và num_str:",num_sum)
+print("Kiểu dữ liệu của sum:",type(num_sum))
+
+a = 1
+b = 2
+print("Cộng: " + str(a + b))
+print("Trừ: " + str(a - b))
+print("Nhân: " + str(a * b))
+print("Chia: " + str(a / b))
+print("Chia lấy dư: " + str(a % b))
+print("Số mũ: " + str(a ** b)) """
